bbed8d0729ff16475652a8e8d6bc1711d0d876b2
Kyle Strayer <kstrayer97@github.com>
2018-03-01 11:51:19 -0700

    Added working CalcAdjacencies function

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -10,14 +10,41 @@ public class IntBoard {
 		calcAdjacencies();
 	}
 	void calcAdjacencies(){
-		for(int i = 0; i < grid.length; i++) {
+		for(int i = 0; i < grid.length; i++) { // i equals columns j = rows 
 			for (int j = 0; j < grid[i].length; j++) {
-				Set adjacencySet = new HashSet<BoardCell>();
-				if (i-1 >= 0) {
-					// TODO: actually add to adjacencySet
-					adjacentTileMap.put(grid[i][j], adjacencySet);
+				Set adjSet = new HashSet<BoardCell>(); // adjacent set for this for loop
+				if ( i == 0){ // handles top of the board
+					adjSet.add(grid[i+1][j]);
+					if (j+1 <= grid[i].length){
+						adjSet.add(grid[i][j+1]);
+					}
+					if (j-1 >= 0){
+						adjSet.add(grid[i][j-1]);
+					}
 				}
-				else continue;
+				else if (i == grid.length){ // handles bottom
+					adjSet.add(grid[i-1][j]);
+					if (j+1 <= grid[i].length){
+						adjSet.add(grid[i][j+1]);
+					}
+					if (j-1 >= 0){
+						adjSet.add(grid[i][j-1]);
+					}
+					
+				}
+				else {
+					adjSet.add(grid[i+1][j]);
+					adjSet.add(grid[i-1][j]);
+					if (j+1 <= grid[i].length){
+						adjSet.add(grid[i][j+1]);
+					}
+					if (j-1 >= 0){
+						adjSet.add(grid[i][j-1]);
+					}
+				}
+				adjacentTileMap.put(grid[i][j], adjSet); // sets the adjSet to the corrisponding grid space
+					
+				}
 			}
 		}
 	}
