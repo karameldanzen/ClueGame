982440be9eaca2d18df4860c5448163432caa118
ROXIT1 <ROXIT1@ROXITs-MacBook-Pro-455.local>
2018-03-13 10:25:44 -0600

    Added baseline for BadConfigFormatException (unimplemented)

---
+++ ClueGame/experiment/BadConfigFormatException.java
@@ -0,0 +1,5 @@
+package experiment;
+
+public class BadConfigFormatException extends Exception {
+	
+}
---
+++ ClueGame/experiment/BoardCell.java
@@ -0,0 +1,10 @@
+package experiment;
+
+public class BoardCell {
+	int row;
+	int col;
+public BoardCell(int a, int b){
+	row = b;
+	col = a;
+}
+}
